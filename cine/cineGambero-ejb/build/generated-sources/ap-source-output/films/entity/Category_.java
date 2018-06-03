8d03ff9c629870237d12a5eca689a8c5b5393876
Fran Gambero <frangansal.13@gmail.com>
2018-06-03 21:53:22 +0200

    Add files via upload

--- cine/cineGambero-ejb/build/generated-sources/ap-source-output/films/entity/Category_.java
+++ cine/cineGambero-ejb/build/generated-sources/ap-source-output/films/entity/Category_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-03T20:30:25")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-03T21:44:45")
 @StaticMetamodel(Category.class)
 public class Category_ { 
 
--- cine/cineGambero-ejb/build/generated-sources/ap-source-output/films/entity/Rating_.java
+++ cine/cineGambero-ejb/build/generated-sources/ap-source-output/films/entity/Rating_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-03T20:30:25")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-03T21:44:45")
 @StaticMetamodel(Rating.class)
 public class Rating_ { 
 
